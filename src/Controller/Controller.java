51a0804faf8e0dcb3b6f813dc7f43711991ad29a
José María Muñoz Quijada <JoseMariaMQ@users.noreply.github.com>
2020-10-31 15:10:24 +0100

    Creación de botón para mostrar miembro de grupo.

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,34 @@
 package Controller;
 
-public class Controller {
+import Model.Model;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
+public class Controller implements EventHandler<ActionEvent> {
+
+    @FXML
+    private Button btnMiembro1;
+    @FXML
+    private Label titleMiembro1;
+    @FXML
+    private Label textMiembro1;
+
+    Model miembro1 = new Model("Miembro 1:", "José María Muñoz Quijada");
+
+    public void initialize() {
+        btnMiembro1.setOnAction(this);
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        if((Button)actionEvent.getSource() == btnMiembro1) {
+            titleMiembro1.setText(miembro1.getTitle());
+            textMiembro1.setText(miembro1.getMiembro());
+        }
+    }
 }
--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,4 +1,27 @@
 package Model;
 
 public class Model {
+    private String title;
+    private String miembro;
+
+    public Model(String title, String miembro) {
+        this.title = title;
+        this.miembro = miembro;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMiembro() {
+        return miembro;
+    }
+
+    public void setMiembro(String miembro) {
+        this.miembro = miembro;
+    }
 }
